d0f2a0d0b28b173bf25789b5c4fe6f8029215bca
Gwabix <50367170+Gwabix@users.noreply.github.com>
2026-08-03 15:34:19 +0200

    minor update

--- TDB-Ecoles/script.js
+++ TDB-Ecoles/script.js
@@ -1096,7 +1096,7 @@ function buildDechargeSelectCell(record, field, options) {
         if (closeButton) {
             const buttonSize = 18;
             closeButton.style.top = Math.round(top - 8) + 'px';
-            closeButton.style.left = Math.round(left + box.width - buttonSize - 4) + 'px';
+            closeButton.style.left = Math.round(left + box.width - 8) + 'px';
         }
     };
 
